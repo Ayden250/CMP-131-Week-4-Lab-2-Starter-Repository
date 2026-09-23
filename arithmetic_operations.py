f67f1de4dfc8ae1d836e227baa4127358acc9248
Ayden250 <suarez.ayden@student.ccm.edu>
2026-09-23 10:20:52 -0400

    Add arithmetic operations in arithmetic_operations.py

---
+++ arithmetic_operations.py
@@ -0,0 +1,22 @@
+#Ayden Suarez
+#Lab 0-2
+#Week 0-4
+#Calculations
+int(input(Number_1))
+int(input(Number_2))
+num_1= 5
+num_2= 3
+print(num_1+num_2)
+num_3= 20
+num_4= 12
+print(num_3-num_4)
+num_5= 25
+num_6= 5
+print(num_5//num_6)
+num_7= 30
+num_8= 9
+print(num_7*num_8)
+num_9= 10
+num_10= 2
+print(num_9**num_10)
+print(num_3+num_9)/2
